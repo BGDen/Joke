1a4252aa205a7b4b7449533b5553d43136715b61
Den <fentau.line@gmail.com>
2020-04-24 17:59:51 +0300

    Create a React app and publish it to GitHub Pages

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import './App.css';
+
+class ListJoke extends React.Component {
+  render() {
+    return (
+      <ol className="list">
+        {this.props.arrJokes.map((el, i) => {
+          return <li key={el.id} value={this.props.arrJokes.length - i} dangerouslySetInnerHTML={{ __html: el.joke }}></li>
+        })
+        }
+      </ol >
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      arrJokes: []
+    };
+    this.addNewJoke = this.addNewJoke.bind(this);
+  }
+
+  checkNewJoke(id) {
+    if (this.state.arrJokes.find(el => el.id === id)) {
+      return true;
+    }
+    return false;
+  }
+
+  async addNewJoke(ev) {
+    ev.preventDefault();
+    let data = {};
+    do {
+      data = await fetch('https://api.icndb.com/jokes/random/').then(res => res.json());
+    } while (this.checkNewJoke(data.value.id))
+
+    const newJoke = { id: data.value.id, joke: data.value.joke };
+    this.state.arrJokes.unshift(newJoke);
+    this.setState(state => ({
+      arrJokes: state.arrJokes.concat()
+    }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Отжиг от Чака :)</h1>
+        <button onClick={this.addNewJoke}>Ещё разочек ?</button>
+        <hr />
+        <ListJoke arrJokes={this.state.arrJokes} />
+        <div className='hidden' />
+      </div >
+    );
+  }
+}
+
+export default App;
