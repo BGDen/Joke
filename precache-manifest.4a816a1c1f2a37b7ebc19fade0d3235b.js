3815f9e764c97135d55a4c7e4de770d7d700e841
Den <fentau.line@gmail.com>
2020-04-24 17:58:49 +0300

    Updates

---
+++ precache-manifest.4a816a1c1f2a37b7ebc19fade0d3235b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1255214dfad82ac3e9d571866c374bf8",
+    "url": "/Joke/index.html"
+  },
+  {
+    "revision": "8a5e9c27cc06ce48e790",
+    "url": "/Joke/static/css/main.045c33db.chunk.css"
+  },
+  {
+    "revision": "1aa35d9ca63fd708ffd2",
+    "url": "/Joke/static/js/2.34e0e667.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Joke/static/js/2.34e0e667.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a5e9c27cc06ce48e790",
+    "url": "/Joke/static/js/main.090b0879.chunk.js"
+  },
+  {
+    "revision": "461451cc51ca2ee4b1ac",
+    "url": "/Joke/static/js/runtime-main.5b309aaa.js"
+  }
+]);
